ee3d644c4624626ecc1f97c6aafab6bdcf0cc543
laura <lpersich@uwaterloo.ca>
2022-01-31 13:41:55 -0500

    update the steps list

--- models/recipe.js
+++ models/recipe.js
@@ -3,7 +3,7 @@ const recipeSchema = mongoose.Schema({
     name: String,
     difficulty: String,
     time_required: String,
-    steps_list: String,
+    steps_list: [String],
     description: String,
     ingredients_list: String,
     image: String
